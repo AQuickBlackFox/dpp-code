6fc447d28fa9124de12577e42a819b32b4bc0dda
Aditya Atluri <Aditya.Atluri@amd.com>
2017-05-25 19:36:25 -0500

    added a working reduction with dpp ops

--- src/reduce/reduce.cpp
+++ src/reduce/reduce.cpp
@@ -2,24 +2,62 @@
 #include<hip/hip_runtime.h>
 #include<iostream>
 
-#define LEN 32
+#define LEN 1024
 #define SIZE LEN << 2
 
-extern __attribute__((const)) int __mov_b32(int, int, int, int, bool) __asm("llvm.amdgcn.mov.dpp.i32");
+extern "C" void __hip_hc_cu_reducex64_f32(float, float);
 
-__global__ void ReduceLane16(int *A, int *B)
+template<typename T, unsigned int blockSize, bool nIsPow2>
+__global__ void ReduceLane64(T *i_data, T *o_data, unsigned int n)
 {
-    int tx = hipThreadIdx_x;
-    B[tx] = __mov_b32(A[tx], 0x111, 1, 1, 1);
+    unsigned int tid = hipThreadIdx_x;
+    unsigned int i  = hipBlockIdx_x * blockSize * 2 + hipThreadIdx_x;
+    unsigned int gridSize = blockSize * 2 * hipGridDim_x;
+
+    T sum = 0;
+    T val = o_data[tid];
+
+    __shared__ T sdata[blockSize];
+
+    while(i < n) {
+        sum += i_data[i];
+        if(nIsPow2 || i + blockSize < n) {
+            sum += i_data[i+blockSize];
+        }
+        i += gridSize;
+    }
+
+    sdata[tid] = sum;
+    __syncthreads();
+
+    if((blockSize >= 512) && (tid < 256)){
+        sdata[tid] = sum = sum + sdata[tid + 256];
+    }
+    __syncthreads();
+
+
+    if((blockSize >= 256) && (tid < 128)){
+        sdata[tid] = sum = sum + sdata[tid + 128];
+    }
+    __syncthreads();
+
+
+    if((blockSize >= 128) && (tid < 64)){
+        sdata[tid] = sum = sum + sdata[tid + 64];
+    }
+    __syncthreads();
+
+    __hip_hc_cu_reducex64_f32(val, sdata[tid]);
+    if(tid == 63) { o_data[hipBlockIdx_x] = val; }
 }
 
 int main(){
-    int *A, *B, *Ad, *Bd;
-    A = new int[LEN];
-    B = new int[LEN];
+    float *A, *B, *Ad, *Bd;
+    A = new float[LEN];
+    B = new float[LEN];
 
     for(int i=0;i<LEN;i++) {
-        A[i] = i;
+        A[i] = 1.0f;
         B[i] = 0;
     }
 
@@ -29,11 +67,12 @@ int main(){
     hipMemcpy(Ad, A, SIZE, hipMemcpyHostToDevice);
     hipMemcpy(Bd, B, SIZE, hipMemcpyHostToDevice);
 
-    hipLaunchKernelGGL(ReduceLane16, dim3(1,1,1), dim3(LEN,1,1), 0, 0, Ad, Bd);
+    hipLaunchKernelGGL((ReduceLane64<float, 512, true>), dim3(LEN/512,1,1), dim3(512,1,1), 0, 0, Ad, Bd, LEN);
 	hipDeviceSynchronize();
     hipMemcpy(B, Bd, SIZE, hipMemcpyDeviceToHost);
 
-    for(int i=0;i<LEN;i++) { std::cout<< B[i] << std::endl; }
+    for(int i=0;i<4;i++) { std::cout<< B[i] << std::endl; }
+    std::cout<<B[0]<<std::endl;
 	delete A;
 	delete B;
 	hipFree(Ad);
